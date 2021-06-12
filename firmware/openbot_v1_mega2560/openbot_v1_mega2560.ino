f9f9e9650048bbb9027ec2248246626e554bd02b
cnctechnet <acer5502@gmail.com>
2021-06-12 17:33:27 +0800

    Update openbot_v1_mega2560.ino

--- firmware/openbot_v1_mega2560/openbot_v1_mega2560.ino
+++ firmware/openbot_v1_mega2560/openbot_v1_mega2560.ino
@@ -78,13 +78,13 @@
   #define PIN_PWM_L2 6
   #define PIN_PWM_R1 8
   #define PIN_PWM_R2 7
-  #define PIN_SPEED_L 2
-  #define PIN_SPEED_R 3
-  #define PIN_VIN A8
-  #define PIN_TRIGGER 29
-  #define PIN_ECHO 28
-  #define PIN_LED_LB 4
-  #define PIN_LED_RB 7
+  #define PIN_SPEED_L 2//测速
+  #define PIN_SPEED_R 3//测速
+  #define PIN_VIN A8//测电压
+  #define PIN_TRIGGER 29//测距离
+  #define PIN_ECHO 28//测距离
+  #define PIN_LED_LB 4//指示灯
+  #define PIN_LED_RB 7//指示灯
 #elif (OPENBOT == PCB_V1)
   #define PIN_PWM_L1 9
   #define PIN_PWM_L2 10
